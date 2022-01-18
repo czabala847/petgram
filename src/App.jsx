69d042a2f90470e88a00d22f5c8d15c354e6faa1
Carlos Zabala <czabala847@gmail.com>
2022-01-18 14:10:31 -0500

    Usando styled components para los estilos

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ListOfCategories } from "./components/ListOfCategories";
+import { GlobalStyle } from "./GlobalStyle";
+
+const App = () => {
+    return (
+        <>
+            <GlobalStyle />
+            <ListOfCategories />
+        </>
+    );
+};
+
+export { App };
